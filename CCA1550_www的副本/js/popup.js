00fe7e6fc896b50c9259f53db5dea0335b69ce0e
cca1550 <83010223+CCA1550@users.noreply.github.com>
2022-08-29 14:18:52 +0800

    Upload

---
+++ CCA1550_www的副本/js/popup.js
@@ -0,0 +1,9 @@
+function popup() {
+	var txt;
+	if (confirm("点击跳转功能当前不可用！")) {
+		txt = "点击跳转功能当前不可用！";
+	} else {
+		txt = "点击跳转功能当前不可用！";
+	}
+	document.getElementById("popup_sent").innerHTML = txt;
+}
